7d294b94ccc87e789e6a6a245a8db15c4cd380d9
Willian <willian.alves.b15@gmail.com>
2022-11-02 17:38:57 -0300

    fix: styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,23 +7,25 @@ const Home = (props) => {
     <div className="home-container">
       <div className="home-containercalculadora">
         <div className="home-container1">
+        <span className="home-text">Text</span>
           <input
             type="text"
             placeholder="VALOR"
-            className="home-textinput input"
+            className="home-textinput"
           />
-          <span className="home-text">Text</span>
         </div>
         <div className="home-container2">
-          <button className="home-button button">Button</button>
-          <button className="home-button1 button">Button</button>
-          <button className="home-button2 button">Button</button>
-          <button className="home-button3 button">Button</button>
-          <button className="home-button4 button">Button</button>
+          <button className="home-button button">5%</button>
+          <button className="home-button1 button">10%</button>
+          <button className="home-button2 button">20%</button>
+          <button className="home-button3 button">30%</button>
+          <button className="home-button4 button">40%</button>
           <input type="text" placeholder="placeholder" className="input" />
         </div>
-        <button className="home-button5 button">Button</button>
-        <button className="home-button6 button">Button</button>
+        <div className="buttons5-6">
+          <button className="home-button5 button">Aumentar</button>
+          <button button className="home-button6 button">Diminuir</button>
+        </div>
       </div>
     </div>
   )
