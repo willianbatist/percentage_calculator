f30f93ce567e4d873a2376e3efe5993a1ab05959
Willian <willian.alves.b15@gmail.com>
2022-11-03 18:39:17 -0300

    feat: colocando os botões para funcionar

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState} from 'react'
 import '../styles/home.css'
 import '../styles/button.css'
 
 const Home = (props) => {
+  const [percentage, setPercentage] = useState(0);
+  const [inputPercentage, setInputPercentage] = useState('');
+
+  const handlePercentageChange = (e) => {
+    setPercentage(e.target.value);
+  }
+
+  const handleInputChange = (e) => {
+    setInputPercentage(e.target.value);
+  }
+
   return (
     <div className="home-container">
       <div className="home-containercalculadora">
         <div className="home-container1">
         <p className="home-text">1111</p>
           <input
+            onChange={ (e) => handleInputChange(e) }
+            value={ inputPercentage }
             type="number"
             placeholder="VALOR"
             className="home-textinput"
           />
         </div>
         <div className="home-container2">
-          <button className="home-button btn-act">5%</button>
-          <button className="home-button1 btn-act">10%</button>
-          <button className="home-button2 btn-act">20%</button>
-          <button className="home-button3 btn-act">30%</button>
-          <button className="home-button4 btn-act">40%</button>
-          <input type="number" placeholder="  %" className="input" />
+          <button onClick={ (e) => handlePercentageChange(e) } className="home-button btn-act" value={ 5 }>5%</button>
+          <button onClick={ (e) => handlePercentageChange(e) } className="home-button1 btn-act" value={ 10 }>10%</button>
+          <button onClick={ (e) => handlePercentageChange(e) } className="home-button2 btn-act" value={ 20 }>20%</button>
+          <button onClick={ (e) => handlePercentageChange(e) } className="home-button3 btn-act" value={ 30 } >30%</button>
+          <button onClick={ (e) => handlePercentageChange(e) } className="home-button4 btn-act" value={ 40 }>40%</button>
+          <input  onChange={ (e) => handlePercentageChange(e) } type="number" placeholder="  %" className="input"/>
         </div>
         <div className="buttons5-6">
           <button className="home-button5 btn2-act">Aumentar</button>
