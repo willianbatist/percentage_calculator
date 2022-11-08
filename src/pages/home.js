02a038537b597efcb26089e0c94649517865d765
Willian <willian.alves.b15@gmail.com>
2022-11-08 14:52:52 -0300

    fix: refatorando percentageCalculation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useState} from 'react';
 import { handleButtonsEvents, handleInputChange, handleInput } from '../hooks/buttons';
+import { percentageCalculation } from '../hooks/calculationLogic';
 import '../styles/home.css';
 import '../styles/button.css';
 
@@ -7,18 +8,6 @@ const Home = (props) => {
   const [percentage, setPercentage] = useState(0);
   const [number, setNumber] = useState('');
   const [result, setResult] = useState(0);
-
-  function percentageCalculation(e, number, percentageValue) {
-    const value = e.target.value;
-    if (value === 'Aumentar') {
-      let sum = (Number(number) * Number(percentageValue)/100) + Number(number);
-      setResult(sum);
-    }
-    if (value === 'Diminuir') {
-      let sum = Number(number) - (Number(number) * Number(percentageValue)/100);
-      setResult(sum);
-    }
-  }
 
   return (
     <div className="home-container">
@@ -42,8 +31,8 @@ const Home = (props) => {
           <input onChange={ (e) => handleInput(e, setPercentage) } type="number" placeholder="     %" className="input"/>
         </div>
         <div className="buttons5-6">
-          <button onClick={ (e) => percentageCalculation(e, number, percentage) } type="button" className="home-button5 btn2-act" value="Aumentar">Aumentar</button>
-          <button onClick={ (e) => percentageCalculation(e, number, percentage) } type="button" className="home-button6 btn2-act" value="Diminuir">Diminuir</button>
+          <button onClick={ (e) => percentageCalculation(e, number, percentage, setResult) } type="button" className="home-button5 btn2-act" value="Aumentar">Aumentar</button>
+          <button onClick={ (e) => percentageCalculation(e, number, percentage, setResult) } type="button" className="home-button6 btn2-act" value="Diminuir">Diminuir</button>
         </div>
       </div>
     </div>
